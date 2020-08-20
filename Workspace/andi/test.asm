8527d33d1b517b32d19b215ce1cd8155e08e3eab
tomiir <tomas.rocchi@zerf.com.ar>
2020-08-19 22:57:54 -0300

    Fix j tests

--- Workspace/andi/test.asm
+++ Workspace/andi/test.asm
@@ -1,20 +1,23 @@
 # Que funciona correctamente #
 ##############################
-# t1 debe valer FFFF siempre
+# t1 vale 0 al hacer un and con un 0
+addi $t1, $t0, 1
 andi $t1,$t0, 0 
 
-# t1 debe valer FFFF siempre
+# t1 debe valer 1 siempre
 li $t0, 1
-andi $t1, $t0   
+andi $t1, $t0, 1  
 
 # t1 debe valer 001 siempre  // 101&011 -> 001
+li $t1, 0
 li $t0, 5
 andi $t1, $t0, 3
 
 
 # t1 puede ser usado en otra instruccionn correctamente
 # t1 debe valer 6 siempre
-li $t0, 5
+li $t0, 7
+li $t1, 0
 andi $t1, $t0, 3
 add $t1, $t1, $t0 
 
--- Workspace/j/test.asm
+++ Workspace/j/test.asm
@@ -2,12 +2,12 @@
 ##############################
 
 # Pc debe valer 4 despues de ejecutarse jump -> no se debe ejecutar el add || en pipeline no se deben ejecutar nunca los add
-li $t1, 2
-li $t0, 3
+li $t1, 8
+li $t0, 16
 j $t1,$t0 
-add $t1, 2
-sub $t0, 2
-nop
+addi $t1, $t0, 2
+subi $t0, $t0, 2
+addi $t0, $t0, 1
 
 # Que las branches funcionan correctamente
 start:
@@ -17,7 +17,7 @@ start:
   add $t0, $t0, $t1
   sub $t0, $t0, $t1
 equal:
-  nop
+  addi $t0, 1
   b start
 
 
@@ -33,13 +33,13 @@ equal:
   li $t0, 0
   j $t0, $t0
 
-start:
-  li $t0, 3
-  li $t1, 2
+start2:
+  li $t0, 8
+  li $t1, 12
   j $t0, $t1
   add $t0, $t0, $t1
   sub $t0, $t0, $t1
-equal:
+equal2:
   nop
   li $t0, 0
   beq $t0, $t0
